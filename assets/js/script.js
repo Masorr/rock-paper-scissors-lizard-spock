64090eed1898cf38b993ccb38fcb32e151b116ed
Masorr <>
2023-08-17 17:27:56 +0000

    Change options variable from an object  to an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,9 @@ let computerScore = document.getElementById("computer-score");
 let rounds = document.getElementById("rounds");
 let playerImage = document.getElementById("player-image");
 let computerImage = document.getElementById("computer-image");
-let options = {
-    1: "rock",
-    2: "paper",
-    3: "scissors",
-    4: "lizard",
-    5: "spock"
-};
+let options = ["rock", "paper", "scissors", "lizard", "spock"];
 
 // Console logs for testing
 console.log(options);
 console.log(options[1] + " " + options[2]);
+console.log(buttons.length);
