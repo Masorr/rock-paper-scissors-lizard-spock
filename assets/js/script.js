d0e3e3444afc3363f5bf23dff0b9c7c12a57a32e
Masorr <>
2023-08-17 14:43:59 +0000

    Declare variables for DOM elements

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+/*
+Declare variables for DOM elements, and available options
+*/
+let buttons = document.getElementsByClassName("controls");
+let playerScore = document.getElementById("player-score");
+let computerScore = document.getElementById("computer-score");
+let rounds = document.getElementById("rounds");
+let playerImage = document.getElementById("player-image");
+let computerImage = document.getElementById("computer-image");
+let options = {
+    1: "rock",
+    2: "paper",
+    3: "scissors",
+    4: "lizard",
+    5: "spock"
+};
+
+// Console logs for testing
+console.log(options);
+console.log(options[1] + " " + options[2]);
