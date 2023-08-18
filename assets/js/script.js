8abc9a8764e9cc03ca8e7f8f29a1014c46ee57f7
Masorr <>
2023-08-18 10:29:32 +0000

    Change variables for DOM elements from let to const

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 /*
 Declare variables for DOM elements, and available options
 */
-let buttons = document.getElementsByClassName("controls");
-let playerScore = document.getElementById("player-score");
-let computerScore = document.getElementById("computer-score");
-let rounds = document.getElementById("rounds");
-let playerImage = document.getElementById("player-image");
-let computerImage = document.getElementById("computer-image");
-let options = ["rock", "paper", "scissors", "lizard", "spock"];
+const buttons = document.getElementsByClassName("controls");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const rounds = document.getElementById("rounds");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const options = ["rock", "paper", "scissors", "lizard", "spock"];
 
+// TODO remove for final push
 // Console logs for testing
 console.log(options);
 console.log(options[1] + " " + options[2]);
